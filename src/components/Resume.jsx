361aacf9eff2b5ad58bb7b0df3e107023c8b0147
Jesper Hansson Falkenby <jesper.hansson.f@gmail.com>
2018-09-25 20:25:03 +0200

    Menu buttons now scroll to their sections when clicked.

---
+++ src/components/Resume.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { hot } from 'react-hot-loader';
+import { withStyles } from '@material-ui/core/styles';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import { Document, Page } from 'react-pdf/dist/entry.webpack';
+import IconButton from '@material-ui/core/IconButton';
+
+const options = {
+  cMapUrl: 'cmaps/',
+  cMapPacked: true,
+};
+
+const styles = {
+  titleWhite: {
+    textAlign: 'center',
+    color: '#fff',
+  },
+  bottomLeft: {
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+  },
+  bottomRight: {
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+  },
+  centerFlex: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+};
+
+class Resume extends React.Component {
+  state = {
+    numPages: null,
+    pageNumber: 1,
+  };
+
+  onDocumentLoad = ({ numPages }) => {
+    this.setState({ numPages });
+  };
+
+  handlePrevious = () => {
+    const { numPages, pageNumber } = this.state;
+    const newNumber = pageNumber - 1;
+    this.setState({ pageNumber: newNumber > 0 ? newNumber : numPages });
+  };
+
+  handleNext = () => {
+    const { numPages, pageNumber } = this.state;
+    const newNumber = pageNumber + 1;
+    this.setState({ pageNumber: newNumber <= numPages ? newNumber : 1 });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { pageNumber, numPages } = this.state;
+
+    return (
+      <React.Fragment>
+        <Typography className={classes.titleWhite} variant="display1" gutterBottom>
+          Résumé
+        </Typography>
+        <div className={classes.centerFlex}>
+          <Paper elevation={8} style={{ position: 'relative' }}>
+            <Document
+              file="documents/JHF_cv.pdf"
+              onLoadSuccess={this.onDocumentLoad}
+              options={options}
+            >
+              <Page pageNumber={pageNumber} />
+            </Document>
+            <IconButton
+              style={{ zIndex: 1 }}
+              className={classes.bottomLeft}
+              aria-label="Previous"
+              onClick={this.handlePrevious}
+            >
+              <KeyboardArrowLeftIcon />
+            </IconButton>
+            <IconButton
+              style={{ zIndex: 1 }}
+              className={classes.bottomRight}
+              aria-label="Next"
+              onClick={this.handleNext}
+            >
+              <KeyboardArrowRightIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="subheading" gutterBottom>
+              {pageNumber} / {numPages}
+            </Typography>
+          </Paper>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+Resume.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+
+export default hot(module)(withStyles(styles)(Resume));
--- src/components/routes/HomePage.jsx
+++ src/components/routes/HomePage.jsx
@@ -9,24 +9,14 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Avatar from '@material-ui/core/Avatar';
-import Paper from '@material-ui/core/Paper';
-import { Document, Page } from 'react-pdf/dist/entry.webpack';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
-import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
-import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
 import projectsData from 'data/projects.json';
 import Projects from 'components/Projects';
+import Resume from 'components/Resume';
 import { hot } from 'react-hot-loader';
-
-const options = {
-  cMapUrl: 'cmaps/',
-  cMapPacked: true,
-};
+import { Link, Element } from 'react-scroll';
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
   grow: {
     flexGrow: 1,
   },
@@ -59,112 +49,58 @@ const styles = theme => ({
     display: 'flex',
     justifyContent: 'center',
   },
-  bottomLeft: {
-    position: 'absolute',
-    left: 0,
-    bottom: 0,
-  },
-  bottomRight: {
-    position: 'absolute',
-    right: 0,
-    bottom: 0,
-  },
 });
 
-class HomePage extends React.Component {
-  state = {
-    numPages: null,
-    pageNumber: 1,
-  };
+const HomePage = (props) => {
+  const { classes } = props;
 
-  onDocumentLoad = ({ numPages }) => {
-    this.setState({ numPages });
-  };
-
-  handlePrevious = () => {
-    const { numPages, pageNumber } = this.state;
-    const newNumber = pageNumber - 1;
-    this.setState({ pageNumber: newNumber > 0 ? newNumber : numPages });
-  };
-
-  handleNext = () => {
-    const { numPages, pageNumber } = this.state;
-    const newNumber = pageNumber + 1;
-    this.setState({ pageNumber: newNumber <= numPages ? newNumber : 1 });
-  };
-
-  render() {
-    const { classes } = this.props;
-    const { pageNumber, numPages } = this.state;
-
-    return (
-      <React.Fragment>
-        <CssBaseline />
-        <AppBar color="secondary" position="static">
-          <Toolbar>
-            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="title" color="inherit" className={classes.grow}>
-              Home
-            </Typography>
-            <Button color="inherit">Projects</Button>
-            <Button color="inherit">Résumé</Button>
-          </Toolbar>
-        </AppBar>
-        <main>
-          <div className={classes.odd}>
-            <Avatar src="images/ProfilePic.png" className={classes.avatar} />
-            <div className={classes.titleWhite}>
-              <Typography color="inherit" variant="display3">
-                Jesper Hansson Falkenby
-              </Typography>
-              <Typography color="inherit" variant="display1" gutterBottom>
-                Programmer - Gaming Enthusiast - Tech Freak
-              </Typography>
-            </div>
-          </div>
-          <Projects data={projectsData} />
-          <div className={classes.odd}>
-            <Typography className={classes.titleWhite} variant="display1" gutterBottom>
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar color="secondary" position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="title" color="inherit" className={classes.grow}>
+            Home
+          </Typography>
+          <Button color="inherit">
+            <Link activeClass="active" to="projects" spy smooth offset={50} duration={500}>
+              Projects
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link activeClass="active" to="resume" spy smooth offset={50} duration={500}>
               Résumé
+            </Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.odd}>
+          <Avatar src="images/ProfilePic.png" className={classes.avatar} />
+          <div className={classes.titleWhite}>
+            <Typography color="inherit" variant="display3">
+              Jesper Hansson Falkenby
             </Typography>
-            <div className={classes.centerFlex}>
-              <Paper elevation={8} style={{ position: 'relative' }}>
-                <Document
-                  file="documents/JHF_cv.pdf"
-                  onLoadSuccess={this.onDocumentLoad}
-                  options={options}
-                >
-                  <Page pageNumber={pageNumber} />
-                </Document>
-                <IconButton
-                  style={{ zIndex: 1 }}
-                  className={classes.bottomLeft}
-                  aria-label="Previous"
-                  onClick={this.handlePrevious}
-                >
-                  <KeyboardArrowLeftIcon />
-                </IconButton>
-                <IconButton
-                  style={{ zIndex: 1 }}
-                  className={classes.bottomRight}
-                  aria-label="Next"
-                  onClick={this.handleNext}
-                >
-                  <KeyboardArrowRightIcon />
-                </IconButton>
-                <Typography className={classes.title} variant="subheading" gutterBottom>
-                  {pageNumber} / {numPages}
-                </Typography>
-              </Paper>
-            </div>
+            <Typography color="inherit" variant="display1" gutterBottom>
+              Programmer - Gaming Enthusiast - Tech Freak
+            </Typography>
           </div>
-        </main>
-      </React.Fragment>
-    );
-  }
-}
+        </div>
+        <Element name="projects">
+          <Projects data={projectsData} />
+        </Element>
+        <div className={classes.odd}>
+          <Element name="resume">
+            <Resume />
+          </Element>
+        </div>
+      </main>
+    </React.Fragment>
+  );
+};
 
 HomePage.propTypes = {
   classes: PropTypes.objectOf(PropTypes.string).isRequired,
