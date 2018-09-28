6d1d3a7364860eb61d8635e14a35c0063bba4441
Jesper Hansson Falkenby <jesper.hansson.f@gmail.com>
2018-09-28 21:43:40 +0200

    Highlight section in menu when scrolled over.

--- src/components/routes/HomePage.jsx
+++ src/components/routes/HomePage.jsx
@@ -13,7 +13,7 @@ import aboutData from 'data/about.json';
 import Projects from 'components/Projects';
 import Resume from 'components/Resume';
 import { hot } from 'react-hot-loader';
-import { Link, Element, animateScroll as scroll } from 'react-scroll';
+import { Link, Element, animateScroll as scroll, scrollSpy } from 'react-scroll';
 import Publications from 'components/Publications';
 import publicationsData from 'data/publications.json';
 import classNames from 'classnames';
@@ -76,185 +76,248 @@ const styles = theme => ({
   },
 });
 
-const HomePage = (props) => {
-  const { classes } = props;
+class HomePage extends React.Component {
+  state = {
+    activeScroll: 'home',
+  };
 
-  const scrollToTop = () => {
+  scrollToTop = () => {
     scroll.scrollToTop();
   };
 
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <AppBar color="secondary">
-        <Toolbar>
-          <div style={{ flexGrow: 1 }}>
-            <Button onClick={scrollToTop} color="inherit">
-              <Typography variant="title" color="inherit">
-                Home
-              </Typography>
-            </Button>
-          </div>
-          <Link
-            style={{ display: 'flex' }}
-            activeClass="active"
-            to="projects"
-            spy
-            smooth
-            offset={-30}
-            duration={500}
-          >
-            <Button color="inherit">Projects</Button>
-          </Link>
-          <Link
-            style={{ display: 'flex' }}
-            activeClass="active"
-            to="resume"
-            spy
-            smooth
-            offset={-30}
-            duration={500}
-          >
-            <Button color="inherit">Résumé</Button>
-          </Link>
-          <Link
-            style={{ display: 'flex' }}
-            activeClass="active"
-            to="about"
-            spy
-            smooth
-            offset={-30}
-            duration={500}
-          >
-            <Button color="inherit">About</Button>
-          </Link>
-          <Link
-            style={{ display: 'flex' }}
-            activeClass="active"
-            to="publications"
-            spy
-            smooth
-            offset={-30}
-            duration={500}
-          >
-            <Button color="inherit">Publications</Button>
-          </Link>
-          <Link
-            style={{ display: 'flex' }}
-            activeClass="active"
-            to="contact"
-            spy
-            smooth
-            offset={-30}
-            duration={500}
-          >
-            <Button color="inherit">Contact</Button>
-          </Link>
-        </Toolbar>
-      </AppBar>
-      <main>
-        <div style={{ paddingTop: 120 }} className={classes.odd}>
-          <Avatar src="images/ProfilePic.png" className={classes.avatar} />
-          <div className={classes.white}>
-            <Typography align="center" color="inherit" variant="display3">
-              Jesper Hansson Falkenby
-            </Typography>
-            <Typography align="center" color="inherit" variant="display1" gutterBottom>
-              Programmer - Gaming Enthusiast - Tech Freak
-            </Typography>
-          </div>
-        </div>
-        <Element name="projects">
-          <div className={classes.layout}>
-            <Projects data={projectsData} />
-          </div>
-        </Element>
-        <Element name="resume">
-          <div className={classes.odd}>
-            <div className={classes.layout}>
-              <Resume />
-            </div>
-          </div>
-        </Element>
-        <Element name="about">
-          <div className={classNames(classes.even, classes.layout)}>
-            <Typography align="center" variant="display1" gutterBottom>
-              About
-            </Typography>
-            {aboutData.data
-              ? aboutData.data.about.map(text => (
-                <Typography key={text} variant="subheading" style={{ marginTop: 10 }}>
-                  {text}
+  componentDidMount() {
+    scrollSpy.update();
+  }
+
+  handleSetActive = (to) => {
+    this.setState({
+      activeScroll: to,
+    });
+  };
+
+  handleSetInactive = (to) => {
+    const { activeScroll } = this.state;
+
+    if (activeScroll === to) {
+      this.setState({
+        activeScroll: null,
+      });
+    }
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { activeScroll } = this.state;
+    const linkOffset = -100;
+
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <AppBar color="secondary">
+          <Toolbar>
+            <div style={{ flexGrow: 1 }}>
+              <Button onClick={this.scrollToTop} color="inherit">
+                <Typography variant="title" color="inherit">
+                  Home
                 </Typography>
-              ))
-              : null}
-          </div>
-        </Element>
-        <Element name="publications">
-          <div className={classes.odd}>
-            <div className={classes.layout}>
-              <Publications data={publicationsData.data} />
+              </Button>
+            </div>
+            <Link
+              style={{ display: 'flex' }}
+              activeClass="active"
+              to="projects"
+              spy
+              smooth
+              offset={linkOffset}
+              duration={500}
+              onSetActive={this.handleSetActive}
+              onSetInactive={this.handleSetInactive}
+            >
+              <Button
+                variant={activeScroll === 'projects' ? 'raised' : 'text'}
+                color={activeScroll === 'projects' ? 'primary' : 'inherit'}
+              >
+                Projects
+              </Button>
+            </Link>
+            <Link
+              style={{ display: 'flex' }}
+              activeClass="active"
+              to="resume"
+              spy
+              smooth
+              offset={linkOffset}
+              duration={500}
+              onSetActive={this.handleSetActive}
+              onSetInactive={this.handleSetInactive}
+            >
+              <Button
+                variant={activeScroll === 'resume' ? 'raised' : 'text'}
+                color={activeScroll === 'resume' ? 'primary' : 'inherit'}
+              >
+                Résumé
+              </Button>
+            </Link>
+            <Link
+              style={{ display: 'flex' }}
+              activeClass="active"
+              to="about"
+              spy
+              smooth
+              offset={linkOffset}
+              duration={500}
+              onSetActive={this.handleSetActive}
+              onSetInactive={this.handleSetInactive}
+            >
+              <Button
+                variant={activeScroll === 'about' ? 'raised' : 'text'}
+                color={activeScroll === 'about' ? 'primary' : 'inherit'}
+              >
+                About
+              </Button>
+            </Link>
+            <Link
+              style={{ display: 'flex' }}
+              activeClass="active"
+              to="publications"
+              spy
+              smooth
+              offset={linkOffset}
+              duration={500}
+              onSetActive={this.handleSetActive}
+              onSetInactive={this.handleSetInactive}
+            >
+              <Button
+                variant={activeScroll === 'publications' ? 'raised' : 'text'}
+                color={activeScroll === 'publications' ? 'primary' : 'inherit'}
+              >
+                Publications
+              </Button>
+            </Link>
+            <Link
+              style={{ display: 'flex' }}
+              activeClass="active"
+              to="contact"
+              spy
+              smooth
+              offset={linkOffset - 450}
+              duration={500}
+              onSetActive={this.handleSetActive}
+              onSetInactive={this.handleSetInactive}
+            >
+              <Button
+                variant={activeScroll === 'contact' ? 'raised' : 'text'}
+                color={activeScroll === 'contact' ? 'primary' : 'inherit'}
+              >
+                Contact
+              </Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
+        <main>
+          <div style={{ paddingTop: 120 }} className={classes.odd}>
+            <Avatar src="images/ProfilePic.png" className={classes.avatar} />
+            <div className={classes.white}>
+              <Typography align="center" color="inherit" variant="display3">
+                Jesper Hansson Falkenby
+              </Typography>
+              <Typography align="center" color="inherit" variant="display1" gutterBottom>
+                Programmer - Gaming Enthusiast - Tech Freak
+              </Typography>
             </div>
           </div>
-        </Element>
-        <Element name="contact">
-          <div className={classes.even}>
+          <Element name="projects">
             <div className={classes.layout}>
-              <Contact />
+              <Projects data={projectsData} />
             </div>
+          </Element>
+          <Element name="resume">
+            <div className={classes.odd}>
+              <div className={classes.layout}>
+                <Resume />
+              </div>
+            </div>
+          </Element>
+          <Element name="about">
+            <div className={classNames(classes.even, classes.layout)}>
+              <Typography align="center" variant="display1" gutterBottom>
+                About
+              </Typography>
+              {aboutData.data
+                ? aboutData.data.about.map(text => (
+                  <Typography key={text} variant="subheading" style={{ marginTop: 10 }}>
+                    {text}
+                  </Typography>
+                ))
+                : null}
+            </div>
+          </Element>
+          <Element name="publications">
+            <div className={classes.odd}>
+              <div className={classes.layout}>
+                <Publications data={publicationsData.data} />
+              </div>
+            </div>
+          </Element>
+          <Element name="contact">
+            <div className={classes.even}>
+              <div className={classes.layout}>
+                <Contact />
+              </div>
+            </div>
+          </Element>
+        </main>
+        <footer className={classes.footer}>
+          <div className={classes.columnFlex}>
+            <div
+              className={classes.rowFlex}
+              style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: 100 }}
+            >
+              <IconButton
+                href="https://www.facebook.com/jesper.hansson.f"
+                className={classes.white}
+                aria-label="Facebook"
+              >
+                <Facebook />
+              </IconButton>
+              <IconButton
+                href="https://www.linkedin.com/in/jesperhf92/"
+                className={classes.white}
+                aria-label="Linkedin"
+              >
+                <Linkedin />
+              </IconButton>
+              <IconButton
+                href="https://twitter.com/JesperFalkenby"
+                className={classes.white}
+                aria-label="Twitter"
+              >
+                <Twitter />
+              </IconButton>
+              <IconButton
+                href="https://www.instagram.com/jesperfalkenby/"
+                className={classes.white}
+                aria-label="Instagram"
+              >
+                <Instagram />
+              </IconButton>
+              <IconButton
+                href="https://github.com/hyzor"
+                className={classes.white}
+                aria-label="GitHub"
+              >
+                <GithubBox />
+              </IconButton>
+            </div>
+            <Typography className={classes.white} variant="subheading" align="center" gutterBottom>
+              Copyright © Jesper Hansson Falkenby 2018
+            </Typography>
           </div>
-        </Element>
-      </main>
-      <footer className={classes.footer}>
-        <div className={classes.columnFlex}>
-          <div
-            className={classes.rowFlex}
-            style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: 100 }}
-          >
-            <IconButton
-              href="https://www.facebook.com/jesper.hansson.f"
-              className={classes.white}
-              aria-label="Facebook"
-            >
-              <Facebook />
-            </IconButton>
-            <IconButton
-              href="https://www.linkedin.com/in/jesperhf92/"
-              className={classes.white}
-              aria-label="Linkedin"
-            >
-              <Linkedin />
-            </IconButton>
-            <IconButton
-              href="https://twitter.com/JesperFalkenby"
-              className={classes.white}
-              aria-label="Twitter"
-            >
-              <Twitter />
-            </IconButton>
-            <IconButton
-              href="https://www.instagram.com/jesperfalkenby/"
-              className={classes.white}
-              aria-label="Instagram"
-            >
-              <Instagram />
-            </IconButton>
-            <IconButton
-              href="https://github.com/hyzor"
-              className={classes.white}
-              aria-label="GitHub"
-            >
-              <GithubBox />
-            </IconButton>
-          </div>
-          <Typography className={classes.white} variant="subheading" align="center" gutterBottom>
-            Copyright © Jesper Hansson Falkenby 2018
-          </Typography>
-        </div>
-      </footer>
-    </React.Fragment>
-  );
-};
+        </footer>
+      </React.Fragment>
+    );
+  }
+}
 
 HomePage.propTypes = {
   classes: PropTypes.objectOf(PropTypes.string).isRequired,
