3ccf4508dc2444cfda418568809a8166045843a2
Jesper Hansson Falkenby <jesper.hansson.f@gmail.com>
2019-03-23 19:47:32 +0100

    Use Parallax from react-spring instead.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,16 @@ import { hot } from 'react-hot-loader';
 import HomePage from 'components/routes/HomePage';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import theme from 'styles/theme';
-import { ParallaxProvider } from 'react-scroll-parallax';
 
 function App() {
   return (
-    <ParallaxProvider>
-      <MuiThemeProvider theme={theme}>
-        <HashRouter>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-          </Switch>
-        </HashRouter>
-      </MuiThemeProvider>
-    </ParallaxProvider>
+    <MuiThemeProvider theme={theme}>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </HashRouter>
+    </MuiThemeProvider>
   );
 }
 
--- src/components/routes/HomePage.jsx
+++ src/components/routes/HomePage.jsx
@@ -19,7 +19,7 @@ import Contact from 'components/Contact';
 import { Facebook, Linkedin, Twitter, Instagram, GithubBox } from 'mdi-material-ui';
 import IconButton from '@material-ui/core/IconButton';
 import { Trail } from 'react-spring/renderprops';
-import { Parallax, ParallaxBanner } from 'react-scroll-parallax';
+import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
 import { Link, Element, animateScroll as scroll, scrollSpy } from 'react-scroll';
 import TransitionReveal from 'components/TransitionReveal';
 
@@ -44,9 +44,13 @@ const styles = theme => ({
     display: 'flex',
     justifyContent: 'center',
   },
-  layout: {
-    paddingTop: 120,
-    paddingBottom: 120,
+  pageParent: {
+    height: '100%',
+    display: 'grid',
+    placeItems: 'center',
+  },
+  page: {
+    position: 'relative',
     width: 'auto',
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
@@ -219,203 +223,218 @@ class HomePage extends React.Component {
           </Toolbar>
         </AppBar>
         <main>
-          <ParallaxBanner
-            layers={[
-              {
-                image: 'images/bg6.jpg',
-                amount: 0.1,
-              },
-            ]}
-            style={{ height: '3055.390px' }}
-          >
-            <Parallax y={[0, 80]}>
-              <Avatar src="images/ProfilePic.png" className={classes.avatar} />
-            </Parallax>
-            <div className={classes.white} style={{ marginTop: 32, marginBottom: 128 }}>
-              <Parallax y={[160, 0]}>
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                  <Trail
-                    items={['Jes', 'per ', 'Han', 'sson ', 'Fal', 'ken', 'by']}
-                    from={{ opacity: 0, transform: 'translate3d(0,-120px,0)' }}
-                    to={{ opacity: 1, transform: 'translate3d(0,0px,0)' }}
-                    delay={500}
-                  >
-                    {item => props => (
-                      <Typography align="center" color="inherit" style={props} variant="h1">
-                        {item}
-                      </Typography>
-                    )}
-                  </Trail>
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                  <Trail
-                    items={['Sys', 'tems ', 'dev', 'elo', 'per']}
-                    from={{ opacity: 0, transform: 'translate3d(0,-120px,0)' }}
-                    to={{ opacity: 1, transform: 'translate3d(0,0px,0)' }}
-                    delay={1250}
-                  >
-                    {item => props => (
-                      <Typography align="center" color="inherit" style={props} variant="h4">
-                        {item}
-                      </Typography>
-                    )}
-                  </Trail>
-                </div>
-              </Parallax>
-            </div>
-            <Element name="projects">
-              <div className={classes.layout}>
-                <TransitionReveal>
-                  <Projects data={projectsData} />
-                </TransitionReveal>
-              </div>
-            </Element>
-          </ParallaxBanner>
-          <ParallaxBanner
-            layers={[
-              {
-                image: 'images/bg.jpg',
-                amount: 0.1,
-              },
-            ]}
-            style={{ height: '2219.590px' }}
-          >
-            <Element name="resume">
-              <div className={classes.layout}>
-                <TransitionReveal>
-                  <Resume />
-                </TransitionReveal>
-              </div>
-            </Element>
-            <Element name="about">
-              <div className={classes.layout}>
-                <TransitionReveal>
+          <div style={{ height: '2000px', position: 'relative' }}>
+            <Parallax
+              pages={6}
+              ref={ref => {
+                this.parallax = ref;
+              }}
+            >
+              <ParallaxLayer
+                offset={0}
+                speed={0}
+                style={{ background: 'url(images/bg4.jpg) center center no-repeat' }}
+              >
+                <Avatar
+                  src="images/ProfilePic.png"
+                  className={classes.avatar}
+                  style={{ marginTop: 320 }}
+                />
+                <div className={classes.white} style={{ marginTop: 32, marginBottom: 128 }}>
                   <div style={{ display: 'flex', justifyContent: 'center' }}>
                     <Trail
-                      items={['Ab', 'out']}
+                      items={['Jes', 'per ', 'Han', 'sson ', 'Fal', 'ken', 'by']}
                       from={{ opacity: 0, transform: 'translate3d(0,-120px,0)' }}
                       to={{ opacity: 1, transform: 'translate3d(0,0px,0)' }}
                       delay={500}
                     >
                       {item => props => (
-                        <Typography
-                          className={classes.white}
-                          style={props}
-                          align="center"
-                          variant="h2"
-                          gutterBottom
-                        >
+                        <Typography align="center" color="inherit" style={props} variant="h1">
                           {item}
                         </Typography>
                       )}
                     </Trail>
                   </div>
-                  <Trail
-                    items={aboutData.data.about}
-                    from={{ opacity: 0, transform: 'translate3d(0,-120px,0)' }}
-                    to={{ opacity: 1, transform: 'translate3d(0,0px,0)' }}
-                    delay={1000}
-                  >
-                    {item => props => (
-                      <Typography
-                        className={classes.white}
-                        style={{ ...props, marginTop: 10 }}
-                        variant="subtitle1"
-                      >
-                        {item}
-                      </Typography>
-                    )}
-                  </Trail>
-                </TransitionReveal>
-              </div>
-            </Element>
-          </ParallaxBanner>
-          <ParallaxBanner
-            layers={[
-              {
-                image: 'images/bg.jpg',
-                amount: 0.1,
-              },
-            ]}
-            style={{ height: '2505.830px' }}
-          >
-            <Element name="publications">
-              <div className={classes.layout}>
-                <TransitionReveal>
-                  <Publications data={publicationsData.data} />
-                </TransitionReveal>
-              </div>
-            </Element>
-            <Element name="contact">
-              <div className={classes.layout}>
-                <TransitionReveal>
-                  <Contact />
-                </TransitionReveal>
-              </div>
-            </Element>
-            <div className={classes.columnFlex}>
-              <div
-                className={classes.rowFlex}
-                style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: 100 }}
-              >
-                <IconButton
-                  href="https://www.facebook.com/jesper.hansson.f"
-                  className={classes.white}
-                  aria-label="Facebook"
-                >
-                  <Facebook />
-                </IconButton>
-                <IconButton
-                  href="https://www.linkedin.com/in/jesperhf92/"
-                  className={classes.white}
-                  aria-label="Linkedin"
-                >
-                  <Linkedin />
-                </IconButton>
-                <IconButton
-                  href="https://twitter.com/JesperFalkenby"
-                  className={classes.white}
-                  aria-label="Twitter"
-                >
-                  <Twitter />
-                </IconButton>
-                <IconButton
-                  href="https://www.instagram.com/jesperfalkenby/"
-                  className={classes.white}
-                  aria-label="Instagram"
-                >
-                  <Instagram />
-                </IconButton>
-                <IconButton
-                  href="https://github.com/hyzor"
-                  className={classes.white}
-                  aria-label="GitHub"
-                >
-                  <GithubBox />
-                </IconButton>
-              </div>
-              <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <Trail
-                  items={['Copyright ', '© ', 'Jesper ', 'Hansson ', 'Falkenby ', '2019']}
-                  from={{ opacity: 0, transform: 'translate3d(0,-120px,0)' }}
-                  to={{ opacity: 1, transform: 'translate3d(0,0px,0)' }}
-                  delay={1000}
-                >
-                  {item => props => (
-                    <Typography
-                      style={props}
-                      className={classes.white}
-                      variant="subtitle1"
-                      align="center"
-                      gutterBottom
+                  <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <Trail
+                      items={['Sys', 'tems ', 'dev', 'elo', 'per']}
+                      from={{ opacity: 0, transform: 'translate3d(0,-120px,0)' }}
+                      to={{ opacity: 1, transform: 'translate3d(0,0px,0)' }}
+                      delay={1250}
                     >
-                      {item}
-                    </Typography>
-                  )}
-                </Trail>
-              </div>
-            </div>
-          </ParallaxBanner>
+                      {item => props => (
+                        <Typography align="center" color="inherit" style={props} variant="h4">
+                          {item}
+                        </Typography>
+                      )}
+                    </Trail>
+                  </div>
+                </div>
+              </ParallaxLayer>
+              <ParallaxLayer
+                offset={1}
+                speed={0}
+                style={{ background: 'url(images/bg4.jpg) center center no-repeat' }}
+              >
+                <Element name="projects" className={classes.pageParent}>
+                  <div className={classes.page}>
+                    <TransitionReveal>
+                      <Projects data={projectsData} />
+                    </TransitionReveal>
+                  </div>
+                </Element>
+              </ParallaxLayer>
+              <ParallaxLayer
+                offset={2}
+                speed={0}
+                style={{ background: 'url(images/bg4.jpg) center center no-repeat' }}
+              >
+                <Element name="resume" className={classes.pageParent}>
+                  <div className={classes.page}>
+                    <TransitionReveal>
+                      <Resume />
+                    </TransitionReveal>
+                  </div>
+                </Element>
+              </ParallaxLayer>
+              <ParallaxLayer
+                offset={3}
+                speed={0}
+                style={{ background: 'url(images/bg4.jpg) center center no-repeat' }}
+              >
+                <Element name="about" className={classes.pageParent}>
+                  <div className={classes.page}>
+                    <TransitionReveal>
+                      <div style={{ display: 'flex', justifyContent: 'center' }}>
+                        <Trail
+                          items={['Ab', 'out']}
+                          from={{ opacity: 0, transform: 'translate3d(0,-120px,0)' }}
+                          to={{ opacity: 1, transform: 'translate3d(0,0px,0)' }}
+                          delay={500}
+                        >
+                          {item => props => (
+                            <Typography
+                              className={classes.white}
+                              style={props}
+                              align="center"
+                              variant="h2"
+                              gutterBottom
+                            >
+                              {item}
+                            </Typography>
+                          )}
+                        </Trail>
+                      </div>
+                      <Trail
+                        items={aboutData.data.about}
+                        from={{ opacity: 0, transform: 'translate3d(0,-120px,0)' }}
+                        to={{ opacity: 1, transform: 'translate3d(0,0px,0)' }}
+                        delay={1000}
+                      >
+                        {item => props => (
+                          <Typography
+                            className={classes.white}
+                            style={{ ...props, marginTop: 10 }}
+                            variant="subtitle1"
+                          >
+                            {item}
+                          </Typography>
+                        )}
+                      </Trail>
+                    </TransitionReveal>
+                  </div>
+                </Element>
+              </ParallaxLayer>
+              <ParallaxLayer
+                offset={4}
+                speed={0}
+                style={{ background: 'url(images/bg3.jpg) center center no-repeat' }}
+              >
+                <Element name="publications" className={classes.pageParent}>
+                  <div className={classes.page}>
+                    <TransitionReveal>
+                      <Publications data={publicationsData.data} />
+                    </TransitionReveal>
+                  </div>
+                </Element>
+              </ParallaxLayer>
+              <ParallaxLayer
+                offset={5}
+                speed={0}
+                style={{ background: 'url(images/bg3.jpg) center center no-repeat' }}
+              >
+                <Element name="contact" className={classes.pageParent}>
+                  <div className={classes.page}>
+                    <TransitionReveal>
+                      <Contact />
+                    </TransitionReveal>
+                  </div>
+                  <div className={classes.columnFlex}>
+                    <div
+                      className={classes.rowFlex}
+                      style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: 100 }}
+                    >
+                      <IconButton
+                        href="https://www.facebook.com/jesper.hansson.f"
+                        className={classes.white}
+                        aria-label="Facebook"
+                      >
+                        <Facebook />
+                      </IconButton>
+                      <IconButton
+                        href="https://www.linkedin.com/in/jesperhf92/"
+                        className={classes.white}
+                        aria-label="Linkedin"
+                      >
+                        <Linkedin />
+                      </IconButton>
+                      <IconButton
+                        href="https://twitter.com/JesperFalkenby"
+                        className={classes.white}
+                        aria-label="Twitter"
+                      >
+                        <Twitter />
+                      </IconButton>
+                      <IconButton
+                        href="https://www.instagram.com/jesperfalkenby/"
+                        className={classes.white}
+                        aria-label="Instagram"
+                      >
+                        <Instagram />
+                      </IconButton>
+                      <IconButton
+                        href="https://github.com/hyzor"
+                        className={classes.white}
+                        aria-label="GitHub"
+                      >
+                        <GithubBox />
+                      </IconButton>
+                    </div>
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                      <Trail
+                        items={['Copyright ', '© ', 'Jesper ', 'Hansson ', 'Falkenby ', '2019']}
+                        from={{ opacity: 0, transform: 'translate3d(0,-120px,0)' }}
+                        to={{ opacity: 1, transform: 'translate3d(0,0px,0)' }}
+                        delay={1000}
+                      >
+                        {item => props => (
+                          <Typography
+                            style={props}
+                            className={classes.white}
+                            variant="subtitle1"
+                            align="center"
+                            gutterBottom
+                          >
+                            {item}
+                          </Typography>
+                        )}
+                      </Trail>
+                    </div>
+                  </div>
+                </Element>
+              </ParallaxLayer>
+            </Parallax>
+          </div>
         </main>
       </React.Fragment>
     );
