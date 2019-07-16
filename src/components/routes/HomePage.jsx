108638c67518d074f2feaef0dd3820ce7869dc97
Jesper Hansson Falkenby <jesper.hansson.f@gmail.com>
2019-07-16 17:55:23 +0200

    Resized some images.

--- src/components/routes/HomePage.jsx
+++ src/components/routes/HomePage.jsx
@@ -188,7 +188,7 @@ class HomePage extends React.Component {
               <ParallaxLayer offset={0.2} speed={0.3 * scrollModifier}>
                 <Transition
                   items={
-                    <Avatar src="images/DSC_0035_resized_1024.png" className={classes.avatar} />
+                    <Avatar src="images/DSC_0035_resized_512.png" className={classes.avatar} />
                   }
                   from={{ opacity: 0 }}
                   enter={{ opacity: 1 }}
