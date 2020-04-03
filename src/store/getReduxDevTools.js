7db30dbcf4c328d4650a88cc5a6f8b561bc11eef
Jesper Hansson Falkenby <jesper.hansson.f@gmail.com>
2020-04-03 22:44:49 +0200

    Upgrade packages to latest

--- src/store/getReduxDevTools.js
+++ src/store/getReduxDevTools.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-underscore-dangle */
 export default function getReduxDevTools() {
   const devtoolsEnabled =
-    process.env.NODE_ENV === 'development' && typeof global.devToolsExtension === 'function';
-  const devTools = devtoolsEnabled ? global.devToolsExtension() : f => f;
+    process.env.NODE_ENV === 'development' &&
+    typeof global.__REDUX_DEVTOOLS_EXTENSION__ === 'function';
+  const devTools = devtoolsEnabled ? global.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f;
   return devTools;
 }
